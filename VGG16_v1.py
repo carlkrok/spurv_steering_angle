e66a2228ba800df1d2047908c04ba2ca0cd4df68
Carl Martin Krokeide <carlmk@clab10.idi.ntnu.no>
2018-04-20 19:29:00 +0200

    Added compile model removed flatten layer

--- VGG16_v1.py
+++ VGG16_v1.py
@@ -22,9 +22,9 @@ def model_vgg16_v1(nr_of_untrainable_layers):
     x = Model(inputs=base_model.input, outputs=base_model.output)
     #x = Flatten()(x.output)
 
-    #x.compile(loss='mean_squared_error',
-    #                  optimizer='adam',
-    #                  metrics=['accuracy'])
+    x.compile(loss='mean_squared_error',
+                      optimizer='adam',
+                      metrics=['accuracy'])
 
     # Regression part
     fc1 = Dense(100, activation='relu')(x)
