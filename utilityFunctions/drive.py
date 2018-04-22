23afe700e623c0284eb6f0c307b01d1407f8c8cb
Amalie Heiberg <amalih@clab15.idi.ntnu.no>
2018-04-22 13:05:19 +0200

    importing issues

--- utilityFunctions/drive.py
+++ utilityFunctions/drive.py
@@ -6,11 +6,11 @@ import shutil
 
 import numpy as np
 #import socketio
-import eventlet
-import eventlet.wsgi
-from PIL import Image
-from flask import Flask
-from io import BytesIO
+#import eventlet
+#import eventlet.wsgi
+#from PIL import Image
+#from flask import Flask
+#from io import BytesIO
 
 from keras.models import load_model
 import h5py
